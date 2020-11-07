975f4e92aa3aed77ea8c51625796020c7675108d
Jose Melero Martinez <jmelero08@gmail.com>
2020-11-07 19:47:05 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,19 @@
   
 class Post
-
   attr_accessor :title, :author
+
+  @@all = []
+
   def initialize(title)
     @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
+    self.author.name unless self.author == nil
   end
 end
