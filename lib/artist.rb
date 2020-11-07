52ae43225345e425ce322d3961a7c4bb8c2eb47e
Jose Melero Martinez <jmelero08@gmail.com>
2020-11-07 19:46:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,34 +1,24 @@
 class Artist
-
   attr_accessor :name
-  attr_reader :songs 
-  
-  @@song_count = 0
 
   def initialize(name)
     @name = name
-    @songs = []
+  end
+
+  def songs
+    @songs = Song.all.select {|song| song.artist == self}
   end
 
   def add_song(song)
-    @@song_count += 1
-    @songs << song
     song.artist = self
   end
 
-  def add_song_by_name(name)
-    s = Song.new(name)
-    self.add_song(s)
-    s
+  def add_song_by_name(song)
+    Song.new(song).artist = self
   end
 
-  def song_count
-    self.song.size 
-  end
-  
   def self.song_count
-    @@song_count
+    Song.all.count
   end
 
 end
-
