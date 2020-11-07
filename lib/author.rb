445ca8a74529861e7923dbe7384841346335590b
Jose Melero Martinez <jmelero08@gmail.com>
2020-11-07 19:46:54 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,29 +1,25 @@
   
 class Author
+  attr_accessor :name
 
-  attr_accessor :name, :posts
-
-  @@post_count = 0
-  
   def initialize(name)
     @name = name
-    @posts = []
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
   end
 
   def add_post(post)
-    @posts << post
     post.author = self
-    @@post_count += 1
   end
 
-  def add_post_by_title(title)
-    post = Post.new(title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
+  def add_post_by_title(post)
+    Post.new(post).author = self
   end
 
   def self.post_count
-    @@post_count
+    Post.all.count
   end
+
 end
